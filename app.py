26004ee33e2973713b4a2f7bf6db9f825104aa97
adamdabba02-spec <adamdabba02@gmail.com>
2026-03-25 02:09:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,31 @@ import os
 
 app = Flask(__name__)
 
+# API KEY
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
+# HOME
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# CHAT API
 @app.route("/chat", methods=["POST"])
 def chat():
     user_message = request.form.get("message")
 
     try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "user", "content": user_message}
-            ]
+        response = client.responses.create(
+            model="gpt-4.1-mini",
+            input=user_message
         )
 
-        reply = response.choices[0].message.content
+        reply = response.output_text
         return jsonify({"reply": reply})
 
     except Exception as e:
-        return jsonify({"reply": "Xato: " + str(e)})
+        return jsonify({"reply": "xato: " + str(e)})
 
+# RUN
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
